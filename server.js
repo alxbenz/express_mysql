2f537337bdcc0d7116a8900376ec47ff25760513
Alex Benz <benzalexander@gmx.de>
2019-02-22 17:09:01 +0100

    added basic authentication

--- server.js
+++ server.js
@@ -12,6 +12,36 @@ const port = process.env.PORT;
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('app/public'));
+
+app.use((req, res, next) => {
+  // exclude OPTIONS request from auth
+  if (req.method == 'OPTIONS') {
+    next();
+  }
+  // authentication middleware
+
+  const auth = { login: config.httpuser, password: config.httppwd };
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = new Buffer(b64auth, 'base64').toString().split(':');
+
+  // Verify login and password are set and correct
+  if (
+    !login ||
+    !password ||
+    login !== auth.login ||
+    password !== auth.password
+  ) {
+    res.set('WWW-Authenticate', 'Basic realm="401"'); // change this
+    res.status(401).send('Authentication required.'); // custom message
+    return;
+  }
+
+  // Access granted...
+  next();
+});
+
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', config.corsOrigin);
   res.header('Access-Control-Allow-Credentials', true);
@@ -21,7 +51,13 @@ app.use(function(req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
-  next();
+
+  // intercept OPTIONS method
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
 app.get('/items', (req, res) =>
